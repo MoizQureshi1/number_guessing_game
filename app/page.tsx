668ca2b2c8778c331c04bd43a71198ba11c9ef04
Moiz Qureshi <m.moiz782008@gmail.com>
2024-12-22 00:47:50 +0500

    update
    push

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import NumberGuessing from '@/components/number-guessing'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <NumberGuessing/>
+      </div>
+    </>
+  )
+}
